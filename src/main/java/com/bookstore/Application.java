48316e6e44b4ab75d55e0b1033c124183db67d3c
franz <franzlogan@gmail.com>
2024-03-03 00:03:40 -0500

    Add Application class

---
+++ src/main/java/com/bookstore/Application.java
@@ -0,0 +1,8 @@
+package com.bookstore;
+
+public class Application {
+        public static void main(String[] args) {
+            System.out.println("Hello, Bookstore Management System");
+
+    }
+}
