2e4ca74516f6bff1db478964b6080ae48b7f9c70
franz <franzlogan@gmail.com>
2024-03-16 21:51:06 -0400

    update pom.xml and springframework class dependencies

--- src/main/java/com/bookstore/Application.java
+++ src/main/java/com/bookstore/Application.java
@@ -1,5 +1,8 @@
 package com.bookstore;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 public class Application {
         public static void main(String[] args) {
             System.out.println("Hello, Bookstore Management System");
