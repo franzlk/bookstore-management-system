4ad327345c5f2c26bb82fb58d55c48d5a39ea964
franz <franzlogan@gmail.com>
2024-03-22 10:31:56 -0400

    Book object implementation change

--- backend/src/main/java/com/bookstore/service/BookService.java
+++ backend/src/main/java/com/bookstore/service/BookService.java
@@ -1,38 +1,11 @@
 package com.bookstore.service;
 
 import com.bookstore.model.Book;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class BookService {
+public interface BookService {
+    Book saveBook(Book book);
 
-    private final BookDAO bookDAO;
-
-    @Autowired
-    public BookService(BookDAO bookDAO) {
-        this.bookDAO = bookDAO;
-    }
-
-    public List<Book> getAllBooks() {
-        return bookDAO.getAllBooks();
-    }
-
-    public Book getBookById(int id) {
-        return bookDAO.getBookById(id);
-    }
-
-    public void addBook(Book book) {
-        bookDAO.addBook(book);
-    }
-
-    public void updateBook(Book book) {
-        bookDAO.updateBook(book);
-    }
-
-    public void deleteBook(int id) {
-        bookDAO.deleteBook(id);
-    }
+    List<Book> getAllBooks();
 }
---
+++ backend/src/main/java/com/bookstore/service/BookServiceImpl.java
@@ -0,0 +1,25 @@
+package com.bookstore.service;
+
+import com.bookstore.model.Book;
+import com.bookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
